0f9ff85e65c4213b5fbc18435e6c5fb8f3ecdcf4
Ana Tcaci <anachiscov@gmail.com>
2020-10-15 13:28:44 +0200

    Updates

---
+++ precache-manifest.793f23da316f60a1c9ee7788bb1c0daa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ee4b653d031448e1771d9549010080c",
+    "url": "/Reto_Final.React-API/index.html"
+  },
+  {
+    "revision": "49e026f426a140a0d56a",
+    "url": "/Reto_Final.React-API/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "e1bff14f1d8af45ead4d",
+    "url": "/Reto_Final.React-API/static/css/main.621a9cec.chunk.css"
+  },
+  {
+    "revision": "49e026f426a140a0d56a",
+    "url": "/Reto_Final.React-API/static/js/2.992b56a2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Reto_Final.React-API/static/js/2.992b56a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1bff14f1d8af45ead4d",
+    "url": "/Reto_Final.React-API/static/js/main.3521510b.chunk.js"
+  },
+  {
+    "revision": "b117b52c031f84a21ad4",
+    "url": "/Reto_Final.React-API/static/js/runtime-main.652a8a86.js"
+  }
+]);
